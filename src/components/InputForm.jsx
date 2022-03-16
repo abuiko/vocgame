99446486951fc66e6255e2ac564bc1986bdf0645
Alena Buiko <abuiko.ma@gmail.com>
2022-03-15 23:41:17 -0400

    add words after click

--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
+import { FiEdit } from 'react-icons/fi'
+import { AiOutlineDelete } from 'react-icons/ai'
 
 const Container = styled.div`
-    min-height: 80vh;   
+    min-height: 100vh;   
     display: flex;
     justify-content: center;
     align-items: center;
+    flex-direction: column;
+    width: 600px;
+    margin: auto;
 `
 const Form = styled.div`
-    
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
 `
 const Text = styled.input`
     margin-right: 10px;
@@ -36,15 +44,86 @@ const Button = styled.button`
     }
 `
 
+const Result = styled.div`
+    width: 100%;
+    margin-top: 50px;
+`
+
+const Row = styled.div`
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 20px;
+    padding: 15px 0px;
+   
+`
+const Left = styled.div``
+const Right = styled.div``
+
+const Icons = styled.div`
+    font-size: 22px;
+`
+
 const InputForm = () => {
+    const [word, setWord] = useState("")
+    const [translation, setTranslation] = useState("")
+    const [results, setResults] = useState([])
+
+    const handleText = (e) => {
+        setWord(e.target.value);
+    };
+    const handleTranslation = (e) => {
+        setTranslation(e.target.value);
+    };
+    const addResult = () => {
+
+        if (word !== "" && translation !== "") {
+            const obj = {
+                id: Math.floor(Math.random() * 1000),
+                word: word,
+                translation: translation,
+            };
+
+            setResults([...results, obj]);
+        }
+    }
 
     return (
         <Container>
             <Form>
-                <Text placeholder="Word" required />
-                <Text placeholder="Translation" required />
-                <Button>ADD</Button>
+                <Text
+                    type="text"
+                    name="text"
+                    id="text"
+                    onChange={(e) => handleText(e)}
+                    placeholder="Word"
+                    required />
+                <Text
+                    type="text"
+                    name="translation"
+                    id="translation"
+                    onChange={(e) => handleTranslation(e)}
+                    placeholder="Translation"
+                    required />
+                <Button onClick={addResult}>ADD</Button>
             </Form>
+
+            {results !== [] ? (
+                <Result>
+                    {results.map(item => (
+                        <Row key={item.id}>
+                            <Left>{item.word}</Left>
+                            <Right>{item.translation}</Right>
+                            <Icons>
+                                <FiEdit style={{ marginRight: "20px", cursor: "pointer", color: "green" }} />
+                                <AiOutlineDelete style={{ cursor: "pointer", color: "red" }} />
+                            </Icons>
+                        </Row>
+                    ))
+                    }
+                </Result>
+            ) : null}
+
+
         </Container>
     )
 }
